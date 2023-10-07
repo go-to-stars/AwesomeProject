7931f4e98cc4b1e8129873e8770c42d5905caa7c
go-to-stars <ivanstep@ukr.net>
2023-10-07 15:51:15 +0300

    added start files

---
+++ App.js
@@ -0,0 +1,51 @@
+// import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  View,
+  StatusBar,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import RegistrationScreen from "./src/Screens/RegistrationScreen";
+import LoginScreen from "./src/Screens/LoginScreen";
+import { useFonts } from "expo-font";
+// import { Linking } from "react-native";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./src/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./src/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    // <KeyboardAvoidingView
+    //   // behavior={Platform.OS === "ios" ? "padding" : "height"}
+    //   style={styles.containerKAV}
+    // >
+    //   <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View style={styles.container}>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
+      {/* <StatusBar style="auto" /> */}
+    </View>
+    //   {/* </TouchableWithoutFeedback>
+    // </KeyboardAvoidingView> */}
+  );
+}
+
+const styles = StyleSheet.create({
+  containerKAV: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
