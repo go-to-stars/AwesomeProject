acad97f1e59b6f78ca590832644c532afdbc6948
go-to-stars <ivanstep@ukr.net>
2023-10-07 16:01:39 +0300

    hw2

---
+++ src/Screens/PostsScreen.js
@@ -0,0 +1,18 @@
+import { useState, useEffect, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+  Animated,
+} from "react-native";
+
+export default function PostsScreen() { }
